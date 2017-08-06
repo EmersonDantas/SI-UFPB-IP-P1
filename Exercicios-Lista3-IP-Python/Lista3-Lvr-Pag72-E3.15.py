64fa1a7ef392cf9e5c002490ab86e7bdb5aa1345
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-06 13:15:21 -0300

    Correção E3.15

--- Exercicios-Lista3-IP-Python/Lista3-Lvr-Pag72-E3.15.py
+++ Exercicios-Lista3-IP-Python/Lista3-Lvr-Pag72-E3.15.py
@@ -1,7 +1,7 @@
 #EMERSON DANTAS S.I-IP-P1
 QCFPD=int(input('Quantos cigarros você fuma por dia?'))
 QAF=int(input('Por quantos anos você fumou?'))
-DP1=QCFPD*QAF
-DP2=DP1*10
-DP3=DP2/1440
+DP1=QCFPD*(QAF*365)#QUANTIDADE DE CIGARROS QUE ELE FUMOU NA VIDA
+DP2=DP1*10#QUANTIDADE DE MINUTOS PERDIDOS
+DP3=DP2/1440#DIAS PERDIDOS
 print('Por ter fumado %d cigarros por dia, durante %d anos, você perdeu %d dias de vida.'%(QCFPD, QAF, DP3))
