e1ee5d9159262ea5483688049948d8cfc6471572
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-07 20:38:27 -0300

    Update
    simplificação de código

--- Exercicios-Lista3-IP-Python/Lista3-Lvr-Pag71-E3.8.py
+++ Exercicios-Lista3-IP-Python/Lista3-Lvr-Pag71-E3.8.py
@@ -1,3 +1,3 @@
 #EMERSON DANTAS S.I IP-P1
 VMT=float(input('Valor em metros:'))
-print('Valor convertido em Milímetros:',VMT*100)
+print('Valor convertido em Milímetros:',VMT*1000)
--- Exercicios-Lista3-IP-Python/Lista3-Sld-Pag28-Alg-15.py
+++ Exercicios-Lista3-IP-Python/Lista3-Sld-Pag28-Alg-15.py
@@ -3,34 +3,15 @@
 
 print('Digite o valor em reais:')
 RS= int(input('R$'))
-N100=RS/100
-R1=RS%100
-N50=R1/50
-R2=R1%50
-N10=R2/10
-R3=R2%10
-N5=R3/5
-N1=R3%5
-if N100 != 0:
-    print('Notas de R$100:',int(N100))
-else :
-    print('Notas de R$100:0')
-if N50 != 0:
-    print('Notas de R$50:',int(N50))
-else :
-    print('Notas de R$50:0')
-if N10 != 0:
-    print('Notas de R$10:',int(N10))
-else :
-    print('0')
-if N5 != 0:
-    print('Notas de R$5:',int(N5))
-else :
-    print('Notas de R$5:0')
-if N1 != 0:
-    print('Notas de R$1:',int(N1))
-else :
-    print('Notas de R$1:0')
+N100=int(RS/100)
+R1=int(RS%100)
+N50=int(R1/50)
+R2=int(R1%50)
+N10=int(R2/10)
+R3=int(R2%10)
+N5=int(R3/5)
+N1=int(R3%5)
+print('Notas de R$100: {N100:d}\nNotas de R$50: {N50:d}\nNotas de R$10: {N10:d}\nNotas de R$5: {N5:d}\nNotas de R$1: {N1:d}'.format(N100=N100,N50=N50,N10=N10,N5=N5,N1=N1))
     
 
 
