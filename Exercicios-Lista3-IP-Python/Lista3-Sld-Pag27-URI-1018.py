15028d82302dda930bd18633a16f15020004dd26
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-07 21:26:09 -0300

    update

--- Exercicios-Lista3-IP-Python/Lista3-Sld-Pag27-URI-1018.py
+++ Exercicios-Lista3-IP-Python/Lista3-Sld-Pag27-URI-1018.py
@@ -1,48 +1,14 @@
-RS= 1577
-N100=RS/100
-R1=RS%100
-N50=R1/50
-R2=R1%50
-N20=R2/20
-R3=R2%20
-N10=R3/10
-R4=R3%10
-N5=R3/5
-R5=R3%5
-N2=R5/2
-N1=R5%2
-if N100 != 0:
-    print('Notas de R$100:',int(N100))
-else :
-    print('0')
-
-if N50 != 0:
-          print('Notas de R$50:',int(N50))
-else :
-          print('0')
-if N20 != 0:
-    print('Notas de R$20:',int(N20))
-else :
-    print('0')
-
-if N10 != 0:
-                print('Notas de R$10:',int(N10))
-else :
-                print('0')
-
-if N5 != 0:
-                      print('Notas de R$5:',int(N5))
-else :
-                      print('0')
-if N2 != 0:
-                      print('Notas de R$2:',int(N2))
-else :
-                      print('0')
-
-if N1 != 0:
-    print('Notas de R$1:',int(N1))
-else :
-    print('0')
-    
-
-
+N= 576
+N100=int(N/100)
+R1=int(N%100)
+N50=int(R1/50)
+R2=int(R1%50)
+N20=int(R2/20)
+R3=int(R2%20)
+N10=int(R3/10)
+R4=int(R3%10)
+N5=int(R3/5)
+R5=int(R3%5)
+N2=int(R5/2)
+N1=int(R5%2)
+print('{N100:d} Nota(s) de R$100,00\n{N50:d} Nota(s) de R$50,00\n{N20:d} Nota(s) de R$20,00\n{N10:d} Nota(s) de R$10,00\n{N5:d} Nota(s) de R$5,00\n{N2:d} Nota(s) de R$2,00\n{N1:d} Nota(s) de R$1,00'.format(N100=N100,N50=N50,N20=N20,N10=N10,N5=N5,N2=N2,N1=N1))
