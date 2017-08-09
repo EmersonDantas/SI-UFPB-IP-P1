39c5be4bfaeb9890c21ff8bb7947d211ce5f9219
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-08 22:28:49 -0300

    1.0

--- Exercicios-Lista4-IP-Python/Lista4-Sld-Pag21-URI-1038.py
+++ Exercicios-Lista4-IP-Python/Lista4-Sld-Pag21-URI-1038.py
@@ -1,1 +1,17 @@
 #EMERSON DANTAS S.I IP-P1
+tipo = str.lower(input('Digite, de acordo com o cardápio, qual refeição você quer:\nCardapio:\ncachorro quente;\nx-salada;\nx-bacon;\ntorrada simples;\nrefrigerante.\nDigite aqui:'))
+quantidade = int(input('Digite a quantidade de {tp}:'.format(tp=tipo)))
+if tipo == 'cachorro quente':
+    preco = (quantidade*4)
+elif tipo == 'x-salada':
+    preco = (quantidade*4.50)
+elif tipo == 'x-bacon':
+    preco = (quantidade*5)
+elif tipo == 'torrada simples':
+    preco = (quantidade*2)
+elif tipo == 'refrigerante':
+    preco = (quantidade*1.5)
+else:
+    print('Refeição inválida!')
+    exit()
+print('Total: R$ {p:.2f}'.format(p=preco))
