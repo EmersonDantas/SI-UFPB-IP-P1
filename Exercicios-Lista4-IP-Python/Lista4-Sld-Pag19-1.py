d2f93f7d94fdafa74d12534cbca1fedf347f79dc
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-08 21:12:05 -0300

    1.0

---
+++ Exercicios-Lista4-IP-Python/Lista4-Sld-Pag19-1.py
@@ -0,0 +1,8 @@
+salario = float(input('Digite seu salário atual:'))
+imposto_1000 = 0.17
+imposto_geral = 0.06
+if salario >= 1000:
+    imposto_a_pagar = (salario*imposto_1000)
+else:
+    imposto_a_pagar = (salario*imposto_geral)
+print('Você deverá pagar R${imp:.2f} de impostos.'.format(imp=imposto_a_pagar))
