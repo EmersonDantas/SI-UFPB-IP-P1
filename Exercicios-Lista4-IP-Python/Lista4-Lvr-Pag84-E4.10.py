1bfd2c045e675843e49d8ab262faaa78855dd5c4
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-11 14:57:41 -0300

    1.0

--- Exercicios-Lista4-IP-Python/Lista4-Lvr-Pag84-E4.10.py
+++ Exercicios-Lista4-IP-Python/Lista4-Lvr-Pag84-E4.10.py
@@ -1,1 +1,22 @@
 #EMERSON DANTAS S.I IP-P1
+consumo = float(input('Digite o consumo de energia em kWh:'))
+tipo = str.lower(input('Digite o tipo de instalação conforme a tabela abaixo:\nR para Residências;\nI para indústrias\nC para comércios.\n'))
+if tipo == 'r':
+    if consumo > 500:
+        preco = 0.65
+    else:
+        preco = 0.40
+elif tipo == 'i':
+    if consumo > 5000:
+        preco = 0.60
+    else:
+        preco = 0.55
+elif tipo == 'c':
+    if consumo > 1000:
+        preco = 0.60
+    else:
+        preco = 0.55
+else:
+    print('Tipo de instalação inválido!')
+pagar = consumo*preco
+print('Você deverá pagar R${r:.2f}'.format(r=pagar))
