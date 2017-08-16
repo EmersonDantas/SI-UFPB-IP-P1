03173e8bf84812eadfad2c6b5e3137fbef4853d8
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-16 19:56:46 -0300

    1.0

--- Exercicios-Lista5-IP-Python/PDF 3, lista de exercícios 3/Lista5-Pdf2-E1.py
+++
@@ -1,9 +0,0 @@
-#EMERSON DANTAS S.I IP-P1
-num = float(input('Digite um número:'))
-if num > 0:
-    numres = 'Positivo'
-elif num < 0:
-    numres = 'Negativo'
-else:
-    numres = 'Neutro'
-print('O número {n} é {r}.'.format(n=num,r=numres))
