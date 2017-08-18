5b3b83fdfcada30ac2fc473c84c4c96218f6d01b
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-17 22:42:18 -0300

    1.1

--- Exercicios-Lista5-IP-Python/PDF 3, lista de exercícios 3/Lista5-Pdf3-E3.py
+++ Exercicios-Lista5-IP-Python/PDF 3, lista de exercícios 3/Lista5-Pdf3-E3.py
@@ -1,12 +1,17 @@
 #EMERSON DANTAS S.I IP-P1
 QP = int(input('Quantas pessoas:'))
-QO = int(QP / 42)#1.92
-QOR = QP % 42 #39
-QV = int(QOR / 20)#1.95
-QVR = QOR % 20 #19
+QO = int(QP / 42)
+QOR = QP % 42
+QV = int(QOR / 20)
+QVR = QOR % 20
 if QVR <= 20:
     QV = QV + 1
 VO = QO * 350
 VV = QV * 200
 VT = (VO + VV) / QP
-print('{QO} ônibus e {QV} van(s)\nR${VT:.2f} por pessoa.'.format(QO=QO,QV=int(QV), VT=VT))
+if QV >= 1 and QO <= 0:
+    print('{QV} van(s)\nR${VT:.2f} por pessoa.'.format(QV=int(QV), VT=VT))
+elif QO >=1 and QV <= 0:
+    print('{QO} ônibus\nR${VT:.2f} por pessoa.'.format(QO=QO, VT=VT))
+else:
+    print('{QO} ônibus e {QV} van(s)\nR${VT:.2f} por pessoa.'.format(QO=QO,QV=int(QV), VT=VT))
