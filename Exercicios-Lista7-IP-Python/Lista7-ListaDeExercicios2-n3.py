b9e6930a7538f63acc34e382a47fa83967a63dc1
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-08-31 17:21:13 -0300

    1.1

--- Exercicios-Lista7-IP-Python/Lista7-ListaDeExercicios2-n3.py
+++ Exercicios-Lista7-IP-Python/Lista7-ListaDeExercicios2-n3.py
@@ -1,11 +1,11 @@
-num1 = int(input('Digite o primeiro número: '))#20
-num2 = int(input('Digite o segundo número: '))#4
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
 quantiNumM4 = 0
 if num1 > num2:
     aux = num2
     num2 = num1
     num1 = aux
 for a in range(num1,num2):
-    if a % 4 == 0:
+    if a % 4 == 0 and a > num1 and a < num2:
         quantiNumM4 += 1
 print('{0} múltiplos.'.format(quantiNumM4))
