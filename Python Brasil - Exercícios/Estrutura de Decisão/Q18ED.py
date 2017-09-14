d2ae67877efb2f015618b7fb403e8cf1750c0d3f
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-09-13 21:04:01 -0300

    1.1

--- Python Brasil - Exercícios/Estrutura de Decisão/Q18ED.py
+++ Python Brasil - Exercícios/Estrutura de Decisão/Q18ED.py
@@ -4,18 +4,18 @@ vv = len(data)#Verificando se a data foi inserida sem barras.
 sonum = data.isdigit()#Verificando se existem somente números.
 
 if vv == 8 and sonum:
+    
     dia = int(data[0:2])
     mes = int(data[2:4])
     ano = int(data[4:8])
 
     if dia <= 31 and dia >= 1 and mes >= 1 and mes <= 12 and ano >= 1: #Analizador
         data = True
+        print('A data digitada é válida.')
     else:
         data = False
         print('Data inválida!')
         
-    if data:
-        print('A data digitada é válida.')
 else:
     print('Valores errados!')
 
