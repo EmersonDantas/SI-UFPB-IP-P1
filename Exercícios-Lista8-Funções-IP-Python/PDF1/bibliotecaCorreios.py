57e0433290553fbe418f168e31283ef50c26b0e1
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-09-16 19:43:27 -0300

    1.0 WIP

---
+++ Exercícios-Lista8-Funções-IP-Python/PDF1/bibliotecaCorreios.py
@@ -0,0 +1,70 @@
+pacote = 'pacote'
+documento = 'documento'
+pequena = 'pequena'
+media = 'media'
+grande = 'grande'
+normal = 'normal'
+sedex = 'sedex'
+sedex10 = 'sedex10'
+
+def validaTipoItem(item):
+    item = str.lower(item)
+    if item == pacote or item == documento:
+        return True
+    else:
+        return False
+
+def validaPeso(peso):
+    if peso > 0:
+        return True
+    else:
+        return False
+
+def convertePeso(peso): #Variavel peso é em Kilos
+    gramas = peso * 1000
+    return gramas
+
+def calculaCustoItem(gramas, item):
+
+    if item == pacote:
+        precoItem = gramas * 0.003
+    else:
+        precoItem = gramas * 0.002
+    return precoItem
+
+def validaEmbalagem(embalagem):
+    embalagem = str.lower(embalagem)
+    if embalagem == pequena or embalagem == media or embalagem == grande:
+        return True
+    else:
+        return False
+
+def calculaCustoEmbalagem(embalagem):
+    if embalagem == 'média':
+        embalagem = media
+
+    if embalagem == pequena:
+        precoEmbalagem = 4
+
+    elif embalagem == media:
+        precoEmbalagem = 7
+
+    else:
+        preco = 10
+    return precoEmbalagem
+
+def validaEntrega(entrega):
+    entrega = str.lower(entrega)
+    if entrega == normal or entrega == sedex or entrega == sedex10:
+        return True
+    else:
+        return False
+
+def calculaCustoItem(entrega):
+    if entrega == sedex:
+        precoEntrega = 5
+    elif entrega == sedex10:
+        precoEntrega = 8
+    else:
+        precoEntrega = 0
+    return precoEntrega
---
+++ Exercícios-Lista8-Funções-IP-Python/PDF1/programa1.py
@@ -0,0 +1,19 @@
+import bibliotecaCorreios
+precoTotal = 0
+
+item = str(input('Digite o tipo de item:\n'))
+
+if bibliotecaCorreios.validaTipoItem(item):
+
+    peso = float(input('Digite o peso do {}:\n'.format(item)))
+    
+    if bibliotecaCorreios.validaPeso(peso):
+        
+        gramas = bibliotecaCorreios.convertePeso(peso)
+        r = bibliotecaCorreios.calculaCustoItem(gramas, item)
+        
+        print(precoTotal)
+    else:
+        print('Peso inválido!')
+else:
+    print('Item inválido!')
