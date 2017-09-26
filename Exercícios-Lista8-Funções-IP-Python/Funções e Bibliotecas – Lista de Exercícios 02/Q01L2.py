89f2b33e9023db34d4566cdbe80d607d33def6e5
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-09-26 18:05:28 -0300

    2.1

--- Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/Q01L2.py
+++ Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/Q01L2.py
@@ -1,31 +1,34 @@
 import bibliotecaLista2
+continuar = 'sim'
 listaAOPT = [['leonardo resende','obraLR1',6,'escultura'],['artst1','obra1',1,'quadro'],['artst2','obra2',2,'quadro'],['artst3','obra3',3,'quadro'],['artst4','obra4',4,'quadro'],['leonardo resende','obraLR2',4,'escultura']]
-entrada = str.lower(input('Oque deseja fazer?\n'
-                          '\n'
-                          'A) Procurar um artista;\n'
-                          'B) Quantidade de obras de um artista;\n'
-                          'C) Obter o preço de uma obra;\n'
-                          'D) Obter o tipo de uma obra;\n'
-                          '\n'
-                          '===>'))
+while continuar == 'sim':
+    entrada = str.lower(input('Oque deseja fazer?\n'
+                              '\n'
+                              'A) Procurar um artista;\n'
+                              'B) Quantidade de obras de um artista;\n'
+                              'C) Obter o preço de uma obra;\n'
+                              'D) Obter o tipo de uma obra;\n'
+                              '\n'
+                              '===>'))
 
-if entrada == 'a': #Procurar um artista
-    obra = str.lower(input('Digite o nome da obra:\n'))
-    resultado = ('O(a) artista da obra {} é {}.'.format(obra,bibliotecaLista2.consultaArtista(obra,listaAOPT)))
+    if entrada == 'a': #Procurar um artista
+        obra = str.lower(input('Digite o nome da obra:\n'))
+        resultado = ('O(a) artista da obra {} é {}.'.format(obra,bibliotecaLista2.consultaArtista(obra,listaAOPT)))
 
-elif entrada == 'b': #Saber quantas obras um dado artista tem
-    artista = str.lower(input('Digite o nome do artista:\n'))
-    resultado = ('O(a) artista {} possui {} obras.'.format(artista,bibliotecaLista2.consultaQuantObras(artista,listaAOPT)))
+    elif entrada == 'b': #Saber quantas obras um dado artista tem
+        artista = str.lower(input('Digite o nome do artista:\n'))
+        resultado = ('O(a) artista {} possui {} obras.'.format(artista,bibliotecaLista2.consultaQuantObras(artista,listaAOPT)))
 
-elif entrada == 'c': #Consulta de preço
-    obra = str.lower(input('Digite o nome da obra:\n'))
-    resultado = ('O preço da obra {} é R${:.2f}'.format(obra,bibliotecaLista2.consultaPreco(obra,listaAOPT)))
+    elif entrada == 'c': #Consulta de preço
+        obra = str.lower(input('Digite o nome da obra:\n'))
+        resultado = ('O preço da obra {} é R${:.2f}'.format(obra,bibliotecaLista2.consultaPreco(obra,listaAOPT)))
 
-elif entrada == 'd': #Consulta de tipo
-    tipo = str.lower(input('Digite o nome da obra:\n'))
-    resultado = ('{} é um(a) {}.'.format(tipo,bibliotecaLista2.consultaTipo(tipo,listaAOPT)))
+    elif entrada == 'd': #Consulta de tipo
+        tipo = str.lower(input('Digite o nome da obra:\n'))
+        resultado = ('{} é um(a) {}.'.format(tipo,bibliotecaLista2.consultaTipo(tipo,listaAOPT)))
 
-else:
-    resultado = 'Você digitou um opção incorreta!'
+    else:
+        resultado = 'Você digitou um opção incorreta!'
 
-print(resultado)
+    print(resultado)
+    continuar = str.lower(input('Deseja fazer uma nova consulta? sim ou não:\n'))
