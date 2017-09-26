b1b767313591ccf6c4776ac49cbe25926378ded9
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-09-26 16:57:49 -0300

    1.0.1

--- Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/Q01L2.py
+++ Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/Q01L2.py
@@ -10,27 +10,22 @@ entrada = str.lower(input('Oque deseja fazer?\n'
                           '===>'))
 
 if entrada == 'a': #Procurar um artista
-
     obra = str.lower(input('Digite o nome da obra:\n'))
     resultado = bibliotecaLista2.consultaArtista(obra)
 
 elif entrada == 'b': #Saber quantas obras um dado artista tem
-
     artista = str.lower(input('Digite o nome do artista:\n'))
     resultado = bibliotecaLista2.consultaQuantObras(artista)
 
 elif entrada == 'c': #Consulta de preço
-
     obra = str.lower(input('Digite o nome da obra:\n'))
     resultado = bibliotecaLista2.consultaPreco(obra)
 
 elif entrada == 'd': #Consulta de tipo
-
     tipo = str.lower(input('Digite o nome da obra:\n'))
     resultado = bibliotecaLista2.consultaTipo(tipo)
 
 else:
-
     resultado = 'Você digitou um opção incorreta!'
 
 print(resultado)
