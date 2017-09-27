719a14f9b9776451b4e82d95e2bce1b9ee45c514
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-09-26 21:29:22 -0300

    2.1

--- Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/bibliotecaLista2.py
+++ Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 02/bibliotecaLista2.py
@@ -2,14 +2,14 @@ def consultaPreco(obra,listaAOPT):
     obra = str.lower(obra)
 
     for index in listaAOPT:
-        if obra == index[1]:
+        if obra == str.lower(index[1]):
             return index[2]
 
 def consultaArtista(obra,listaAOPT):
 
     obra = str.lower(obra)
     for index in listaAOPT:
-        if obra == index[1]:
+        if obra == str.lower(index[1]):
             return index[0]
 
 def consultaQuantObras(artista,listaAOPT):
@@ -17,7 +17,7 @@ def consultaQuantObras(artista,listaAOPT):
     cont = 0
     artista = str.lower(artista)
     for index in listaAOPT:
-        if artista == index[0]:
+        if artista == str.lower(index[0]):
             cont += 1
 
     return cont
@@ -26,5 +26,5 @@ def consultaTipo(obra,listaAOPT):
     obra = str.lower(obra)
 
     for index in listaAOPT:
-        if obra == index[1]:
+        if obra == str.lower(index[1]):
             return index[3]
