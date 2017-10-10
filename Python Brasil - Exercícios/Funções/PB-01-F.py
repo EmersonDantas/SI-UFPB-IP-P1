147b0e652d17831dc013278275e8a08439186550
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-10-10 12:18:44 -0300

    1.1

--- Python Brasil - Exercícios/Funções/PB-01-F.py
+++ Python Brasil - Exercícios/Funções/PB-01-F.py
@@ -1,7 +1,7 @@
 n = int(input('Digite um número:\n'))
 def funcao01(n):
     for i in range(1,n+1):
-        for a in range(1,i+1):
-            print(i)
-    return(i)
-print(funcao01(n))
+        for a in range(i):
+            print(i,end='')
+        print()
+funcao01(n)
