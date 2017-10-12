35266c9f4c0c7961e62b3560fd3d60e5b2b6e72c
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-10-12 19:19:36 -0300

    1.0

---
+++ Python Brasil - Exercícios/Listas/PB-24-Dados - L.py
@@ -0,0 +1,12 @@
+from random import randrange
+
+lista = []
+valores = [0,0,0,0,0,0]
+
+for i in range(100):
+    dado = randrange(1, 7)
+    lista.append(dado)
+    valores[dado-1] += 1
+
+for a,face in enumerate(valores):
+    print('{} dados de face número {}'.format(face,a+1))
