c5a1b7a03ddd45bb16b090a6df2fafe38a2b9084
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-10-12 17:43:00 -0300

    1.0

---
+++ Python Brasil - Exercícios/Strings/PB-12-3ro Digito- STR.py
@@ -0,0 +1,20 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+print('Valida e corrige número de telefone')
+numero = str(input('Telefone:   '))
+
+if len(numero.replace('-','')) == 7:
+    numero = numero.replace('-','')
+    print('Telefone possui 7 dígitos. Vou acrescentar o digito três na frente.')
+    numero = '3' + numero
+    print('Telefone corrigido sem formatação: {}\n'.format(numero))    
+    print('Telefone corrigido com formatação: {}-{}'.format(numero[0:4],numero[4:8]))
+          
+    
+elif len(numero.replace('-','')) == 8:
+    print(numero)
+
+else:
+    print('Numero inválido')
