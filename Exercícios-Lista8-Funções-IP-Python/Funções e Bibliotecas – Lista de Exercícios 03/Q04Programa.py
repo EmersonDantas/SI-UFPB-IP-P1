b757cd81ec6c36439890970e8f645fef007b9765
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-10-13 11:28:26 -0300

    1.0

--- Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 03/Q04Programa.py
+++ Exercícios-Lista8-Funções-IP-Python/Funções e Bibliotecas – Lista de Exercícios 03/Q04Programa.py
@@ -1,44 +1,50 @@
-
 revistas = []
 livro = []
-perguntas = ['Qual a editora:','Qual o titulo:','Qual é o preço:','Quantos exemplares vendeu:']
+perguntas = ['Qual a editora:','Qual o titulo:','Qual é o preço:']
 continuar = 'kkk eae men'
 quantiG = 0
 maior = 0
 soma = 0
 cont = 0
 while continuar != 'fim':
-    print('-_-_-_-_-_-ADICIONANDO LIVROS-_-_-_-_-_-\nPara parar digite FIM na primeira pergunta.')
+    print('ADICIONANDO LIVROS\nPara parar digite FIM na primeira pergunta.')
     for p in range(len(perguntas)):
         if p != 2 and p != 3:
-            livro.append(str(input(perguntas[p])))
+            entrada = str(input(perguntas[p]))
+            livro.append(entrada)
         else:
             livro.append(float(input(perguntas[p])))
         if str.lower(livro[0]) == 'fim':
             continuar = 'fim'
             break
-            
+    livro.append(0)
     revistas.append(livro)
     livro = []
-    print('-_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_-\n\n\n')
     if continuar == 'fim':
             del revistas[-1]
 
-for index in revistas:
+for index in revistas: #Quantidade de revistas da editora
     if str.lower(index[0]) == 'globo':
         quantiG += 1
-        
-for index in revistas:
-    temp = index[3]
-    if temp > maior:
-        maior = index[3]
-        nomeMaior = index[1]
-    if temp > 100:
-        cont += 1
-        soma += index[2]
+
+for i in range(len(revistas)): #Titulos iguais, quantidades de venda
+    for a in range(len(revistas)):
+        if revistas[i][1] == revistas[a][1]:
+            revistas[i][3] += 1
+
+maior = revistas[0][3]
+nomeMaior = revistas[0][1]
+maisde100 = 0
+contM100 = 0
+for e in range(len(revistas)): #Titulo mais vendido
+    if revistas[e][3] > maior:
+        maior = revistas[e][3]
+        nomeMaior = revistas[e][1]
+    if revistas[e][3] > 100: #Valor médio dos titulos com mais de 100 vendas
+        maisde100 += revistas[e][2]
+        contM100 += 1
 
 print('Quantidade de revistas da editora globo: {};\n'
-      'Titulo da revista mais vendida: {};\n'
-      'Valor médio das revistas com mais de 100 exemplares vendidos: R${:.2f}'
-      .format(quantiG,nomeMaior,soma/cont))
-        
+      'Titulo da revista mais vendida: {};\n'.format(quantiG,nomeMaior))
+if maisde100 > 0:
+    print('Valor médio das revistas com mais de 100 exemplares vendidos: R${:.2f}'.format(maisde100/contM100))
--- Python Brasil - Exercícios/Listas/PB-21-Comparativo Consumo Carros- L.py
+++ Python Brasil - Exercícios/Listas/PB-21-Comparativo Consumo Carros- L.py
@@ -9,5 +9,5 @@ carrosEconsumo = [['fusca',7],['gol',10],['uno',12.5],['vectra',9],['Peugeout',1
     
 for i,carro in enumerate(carrosEconsumo):
     print('{} - {}     -  {}  -  {:.1f} litros  -  R${:.2f}'.format(i+1,carro[0],carro[1],1000/carro[1],1000/carro[1]*2.25))
-carrosEconsumo.sort(key=lambda x: x[1])
+carrosEconsumo.sort(key=lambda lista: lista[1])
 print('O menor consumo é do {}.'.format(carrosEconsumo[-1][0]))
