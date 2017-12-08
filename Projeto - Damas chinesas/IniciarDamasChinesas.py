744d21235e326804a5b425e3eba073544a167436
Emerson Ruan Dantas Silva <emerson.ruan@dce.ufpb.br>
2017-12-07 21:38:09 -0300

    5.0
    Projeto final da disciplina de introdução a programação do curso de sistemas de informação do campus IV da UFPB, em 2017.1.

---
+++ Projeto - Damas chinesas/IniciarDamasChinesas.py
@@ -0,0 +1,10 @@
+# UFPB IV - SI
+# Emerson Dantas - MATRICULA: 20170050755 - emerson.ruan@dce.ufpb.br - github.com/emersondantas
+# Francivaldo Napoleão - MATRICULA: 20170050610 - francivaldo.napoleao@dce.ufpb.br - github.com/NapoleaoHerculano
+# coding: utf-8
+# outubro/2017
+# Projeto - Damas Chinesas
+
+from GUI import *
+from Motor import *
+IniciarJogo()
